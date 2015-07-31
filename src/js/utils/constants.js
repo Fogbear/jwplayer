512c3d6bd23edb8846a07ad3b4cba0e539765c9d
Fogbear <wjdtjq6940@naver.com>
2015-07-31 19:20:23 +0900

    Revert "1"
    This reverts commit e82bba7d6fd2ab537aef8c0e05ce85ed2e397829.

--- src/js/utils/constants.js
+++
@@ -1,7 +0,0 @@
-define([], function() {
-    return {
-        repo : 'http://p.jwpcdn.com/player/v/',
-        SkinsIncluded : ['seven'],
-        SkinsLoadable : ['beelden', 'bekle', 'five', 'glow', 'roundster', 'six', 'stormtrooper', 'vapor']
-    };
-});
